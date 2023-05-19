d2c2b565f8d58726578d7dd9ef5c787ccc5c419a
Logan Bramwell <loganbramwellqu@gmail.com>
2023-05-18 17:50:46 -0700

    updated handleadd

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,23 @@
+import React, {useState} from 'react';
+import './App.css';
+import ReactModal from 'react-modal';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Popup from 'reactjs-popup';
 
+export default function App(){
 
-export default function app(){
-  let items = [
+  //items should be state
+  //adding item to the list 
+  
+
+  const [items, setItems] = useState([
     {dayabrev:"M", do: "finish homework"},
     {dayabrev:"W", do: "go boxing for three hours"},
     {dayabrev:'TU', do: "run hills for a half hour"},
     {dayabrev:"TH", do: "call mom for mothers day"},
     {dayabrev:"TH", do: "tee time at 8:00 am"},
-  ];
+  ]);
+
 
   const days = [
     { label: "Monday", dayabrev: "M" },
@@ -22,9 +32,10 @@ export default function app(){
   
   return(
     //maps each day to the ToDoList component & displays each day as header
-    <div class = "appwrapper">
+    <div className = "appwrapper">
       <Title/>
       <SearchBar/>
+      <HandleAdd setItems = {setItems} />
       <div>
       {days.map(day => 
         <div key={day.dayabrev}>
@@ -43,13 +54,13 @@ function ToDoList({todos, day}){
   //passes to ToDoItem for css management
   let task = todos.filter(todos => todos.dayabrev === day.dayabrev).map(filteredtask => (
   <li> {filteredtask.do}
-  <button> completed</button>
-  <button> edit </button>
+  <button className = "completed"> completed</button>
+  <button className = "edit"> edit </button>
   </li>));
  
   return(
-    <div class = "taskswrapper">
-      <ul class = "tasks">
+    <div className = "taskswrapper">
+      <ul className = "tasks">
         {task}
       </ul>
     </div>
@@ -58,18 +69,21 @@ function ToDoList({todos, day}){
 
 function Title(){
   return(
-    <div class = "titlewrapper">
-      <h1 class = "title">
+    <div className = "titlewrapper">
+      <h1 className = "title">
         To Do List
       </h1>
     </div>
   )
 }
 
-function SearchBar(){
+function SearchBar({children}){
   return(
-    <div class = "searchwrapper"> 
-      <form class = "search">
+    <div className = "searchwrapper"> 
+    <div className='addwrapper'>
+    {children}
+      </div>
+      <form className = "search">
         <input type = "text" placeholder = "search..." />
         <br/>
         <input type = "checkbox" id = "showall" name = "showall"/>
@@ -78,3 +92,69 @@ function SearchBar(){
     </div>
   )
 }
+
+
+function HandleAdd({setItems}){
+  const [isOpen, setIsOpen] = useState(true);
+  const [newTask, setNewTask] = useState('');
+  const [day, setDay] = useState('');
+
+  const handleSubmit = () => {
+    const task = {
+      dayabrev: day,
+      do: newTask,
+    };
+
+    //update tasks
+    setItems(prevItems => [...prevItems, task]);
+    //clear inputs
+    setDay('');
+    setNewTask('');
+    //close popup
+    setIsOpen(false);
+    
+
+  }
+
+  return(
+    <>
+  <button class = "addtask" onClick={() => setIsOpen(true)}>
+    Add Task
+  </button>
+
+  <ReactModal 
+    isOpen={isOpen}
+    contentLabel='addingtask'
+    onRequestClose={() => setIsOpen(false)}>
+  <div className = 'modalwrap'>
+      <h1>Add Item</h1>
+    
+      <h2 class = 'modalsubtitle'>Task</h2>
+
+      <input 
+        className = "modalinput"
+        type = 'text' 
+        value = {newTask}
+      /* e is event object, "when value of this  input field changes, 
+      apply new inputted value to e, then update newtask "*/
+        onChange={(e) => setNewTask(e.target.value)}/> 
+ 
+      <h2 class = 'modalsubtitle'>Day</h2>
+
+      <input 
+        className='modalinput'
+        type = 'text'
+        value = {day}
+        onChange={(e) => setDay(e.target.value)}/>
+        
+      <button onClick = {handleSubmit} //runs handle submit when click, which adds new task 
+      > 
+        Submit
+      </button>
+  </div>
+  </ReactModal>
+  </>
+
+  )
+}
+
